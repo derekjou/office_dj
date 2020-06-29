7b0e2e3737bd3c9346498c7d69375521f031d10b
Victoria <victoria.hart.design@gmail.com>
2020-06-29 16:53:34 -0400

    helps if you save

--- server/data/mongo.py
+++ server/data/mongo.py
@@ -9,7 +9,7 @@ from pymongo import MongoClient, errors, ReturnDocument
 
 
 try:
-    _db = MongoClient(os.environ.get('MONGO_URI')).las
+    _db = MongoClient(os.environ.get('MONGO_URI')).db
 except:
     _log.exception('Could not connect to Mongo')
     raise
