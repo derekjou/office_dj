ed3d0a11512d434d2e6216097d5cc22e3152644b
TLusby <timothy_lusby@hotmail.com>
2020-06-29 15:39:08 -0700

    started user handler

--- server/app.py
+++ server/app.py
@@ -1,6 +1,10 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, jsonify
+
+from server.model.users import User, DJ
+from server.model.user_handler import user_page
 
 app = Flask(__name__)
+app.register_blueprint(user_page)
 
 @app.route("/")
 def home():
