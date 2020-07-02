d06bfc0f288aa43e7d7191f6e1b901d039a727b2
Derek <derekjou0@gmail.com>
2020-07-02 13:45:02 -0700

    counter added

--- server/data/mongo.py
+++ server/data/mongo.py
@@ -18,9 +18,21 @@ except:
     _log.exception('Could not connect to Mongo')
     raise
 
+def _get_id():
+    '''Retrieves the next id in the database and increments it.'''
+    return _db.counter.find_one_and_update({'_id': 'COUNT'},
+                                            {'$inc': {'count': 1}},
+                                            return_document=ReturnDocument.AFTER)['count']
+
 def add_user(input_user):
     '''a method to add a new user to the database'''
     _log.info("adding user to the database")
     _db.users.insert_one(input_user.to_dict())
     _log.debug(input_user.to_dict())
     return input_user.to_dict()
+
+if __name__ == "__main__":
+    _db.users.drop()
+    _db.counter.drop()
+
+    _db.counter.insert_one({'_id': 'COUNT', 'count': 0})
