6243ca8e7d4d3c161a6d6be678d9ba2fcf6e24ad
Felix Echavarria <echavarria.f@gmail.com>
2020-07-08 17:07:53 -0400

    create the structure to show the data

--- client/src/components/JoinRoom/WorkRequest.js
+++ client/src/components/JoinRoom/WorkRequest.js
@@ -3,10 +3,11 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Form from "react-bootstrap/Form";
-import AdminService from '../../services/admin.service'
+import UserService from '../../services/user.service'
+import Table from 'react-bootstrap/Table'
 const axios = require('axios');
 
-const adminService = new AdminService();
+const userService = new UserService();
 
 const WorkRequest = (props) => {
   const state = useSelector(state => state);
@@ -21,16 +22,48 @@ const WorkRequest = (props) => {
 
   }
 
+  const renderHeader = () => {
+    let headerElement = ['username', 'department', 'team', 'date of request', 'actions']
 
-  return (
-    <>
-      <ListGroup variant="flush">
-        {/* TODO: All the request */}
-        <Button block onClick={approvejoin}>Accept</Button>
-        <Button block onClick={approvejoin}>Reject</Button>
-      </ListGroup>
-    </>
-  )
+    return headerElement.map((key, index) => {
+      return <th key={index}>{key.toUpperCase()}</th>
+    })
+  }
+
+  const renderBody = () => {
+    return (
+      <tr key={username}>
+        <td>{username}</td>
+        <td>{department}</td>
+        <td>{team}</td>
+        <td>{date}</td>
+        <td className='actions'>
+          <Button block onClick={acceptRequest}>
+            Accept
+            </Button>
+          <Button block onClick={rejectRequest}>
+            Reject
+            </Button>
+        </td>
+      </tr>
+    )
+  }
+}
+return (
+  <>
+    <div>
+      <Table responsive="lg" hover>
+        <thead>
+          <tr>{renderHeader()}</tr>
+        </thead>
+        <tbody id="data">
+          {renderBody()}
+        </tbody>
+      </Table>
+    </div>
+
+  </>
+)
 }
 
 export default WorkRequest;
