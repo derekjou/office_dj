b58b3bef5a3bb53e289ef503e8e8d18a3724770d
Felix Echavarria <echavarria.f@gmail.com>
2020-07-07 23:15:36 -0400

    Create the branch and the js file to handle the request for joinning a room

---
+++ client/src/components/JoinRoom/WorkRequest.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Form from "react-bootstrap/Form";
+import AdminService from '../../services/user.service'
+const axios = require('axios');
+
+
+const WorkRequest = (props) => {
+
+  return (
+    <>
+      <ListGroup variant="flush">
+
+      </ListGroup>
+    </>
+  )
+}
+
+export default WorkRequest;
