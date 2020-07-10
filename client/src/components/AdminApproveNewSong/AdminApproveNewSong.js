0baac6dcc4d9e246f4b50d0369bad45c49e8d64d
TLusby <timothy_lusby@hotmail.com>
2020-07-09 21:34:55 -0700

    added preview song button and fixed spelling

--- client/src/components/AdminApproveNewSong/AdminApproveNewSong.js
+++ client/src/components/AdminApproveNewSong/AdminApproveNewSong.js
@@ -33,12 +33,17 @@ const AdminApproveNewSong = (props) => {
         getNewSongRequests()
         }
 
+    function previewSong(request) {
+        window.open(request.url, request.title)
+    }
+
     return (
         <div>
             {state.songRequests.map(request => (
                 <p key={request._id}>
-                    {request.title}
-                    <Button block type="button" onClick={() => {approve(request)}}>Aprove</Button>
+                    Title: {request.title} Artists: {request.artists} Genre: {request.genre} Album: {request.album} 
+                    <Button block type="button" onClick={() => {previewSong(request)}}>Preview Song</Button>
+                    <Button block type="button" onClick={() => {approve(request)}}>Approve</Button>
                     <Button block type="button" onClick={() => {deny(request)}}>Reject</Button>
                 </p>
             ))}
