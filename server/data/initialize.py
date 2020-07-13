c77dad5b2e26dbdb40f3b39e43aa2ab91b9d7086
Felix Echavarria <echavarria.f@gmail.com>
2020-07-12 22:50:34 -0400

    Adding songs to the initialize Database

--- server/data/initialize.py
+++ server/data/initialize.py
@@ -1,24 +1,26 @@
 '''A development module for defining initial database conditions.'''
 
 # External imports
-import os, sys
+from server.data.logger import get_logger
+from server.model.rooms import Room
+from server.songs.model import Song
+from server.model.users import User, DJ, Admin
+from server.data.mongo import _db, _get_id, _get_song_number
+from bson.son import SON
+from pymongo import MongoClient, errors, ReturnDocument
+from datetime import datetime, timedelta
+import os
+import sys
 sys.path.append(os.path.abspath(os.path.join('..')))
 
-from datetime import datetime, timedelta
-from pymongo import MongoClient, errors, ReturnDocument
-from bson.son import SON
 
 # Interal imports
-from server.data.mongo import _db, _get_id, _get_song_number
-from server.model.users import User, DJ, Admin
-from server.songs.model import Song
-from server.model.rooms import Room
-from server.data.logger import get_logger
 
 _log = get_logger(__name__)
 
 if __name__ == "__main__":
-    _log.info('Running Initialize script: dropping collections from _library database')
+    _log.info(
+        'Running Initialize script: dropping collections from _library database')
     _log.info(_db.list_collection_names())
     _db.counter.drop()
     _db.users.drop()
@@ -29,7 +31,6 @@ if __name__ == "__main__":
 
     _db.counter.insert_one({'_id': 'COUNT', 'count': 0})
     _db.counter.insert_one({'_id': 'UNIQUE_SONG_NUMBER', 'count': 0})
-
 
     # USERS #
 
@@ -43,11 +44,11 @@ if __name__ == "__main__":
         'Delevoper'
     ).to_dict())
     user_list.append(User(
-        _get_id(), 
-        'user', 
-        'pass', 
+        _get_id(),
+        'user',
+        'pass',
         'Engineering',
-        'UI', 
+        'UI',
         'Junior Developer'
     ).to_dict())
     user_list.append(Admin(
@@ -56,17 +57,16 @@ if __name__ == "__main__":
         'pass'
     ).to_dict())
 
-
     # SONGS #
 
     song_list = []
     song_list.append(Song(
         _get_song_number(),
-        "La Mujer de Antonio",
-        "SONGO 21 - Studio sessions 2003",
-        ["SONGO 21"],
-        "Salsa",
-        "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/SONGO_21/SONGO_21_-_Studio_sessions_2003/SONGO_21_-_02_-_La_Mujer_de_Antonio.mp3"
+        "Propuesta Indecente",
+        "Propuesta Indecente",
+        ["Romeo Santos"],
+        "Bachata",
+        "https://sharethisurls.com/api/get_song.php?id=cuWBZB:zXt1rB"
     ).to_dict())
     song_list.append(Song(
         _get_song_number(),
@@ -82,9 +82,56 @@ if __name__ == "__main__":
         'Contra La Corriente',
         ['Marc Anthony'],
         'Salsa',
-        'someurl.mp3'
+        'https://sharethisurls.com/api/get_song.php?id=FvWBZB:N111rB'
     ).to_dict())
-
+    song_list.append(Song(
+        _get_song_number(),
+        'El Gran Varon',
+        'Altos Secretos',
+        ['Willie Colon'],
+        'Salsa',
+        'https://sharethisurls.com/api/get_song.php?id=ZtcVvB:uSE2rB'
+    ).to_dict())
+    song_list.append(Song(
+        _get_song_number(),
+        'Set Fire To The Rain',
+        '21',
+        ['Adele'],
+        'Pop',
+        'https://sharethisurls.com/api/get_song.php?id=zuWBZB:Upx1rB'
+    ).to_dict())
+    song_list.append(Song(
+        _get_song_number(),
+        'In The End',
+        'Reanimation',
+        ['Linkin Park'],
+        'Hard Rock',
+        'https://sharethisurls.com/api/get_song.php?id=FvWBZB:hRx1rB'
+    ).to_dict())
+    song_list.append(Song(
+        _get_song_number(),
+        'Uptown Funk',
+        'Uptown Funk',
+        ['Mark Ronson', 'Bruno Mars'],
+        'Funk',
+        'https://sharethisurls.com/api/get_song.php?id=RczquH:PDY1rB'
+    ).to_dict())
+    song_list.append(Song(
+        _get_song_number(),
+        'Dreaming of You',
+        'Dreaming of You',
+        ['Selena'],
+        'Pop',
+        'https://sharethisurls.com/api/get_song.php?id=KvWBZB:V2v1rB'
+    ).to_dict())
+    song_list.append(Song(
+        _get_song_number(),
+        'You Are My God',
+        'N/A',
+        ['Tony Melendez'],
+        'N/A',
+        'https://sharethisurls.com/api/get_song.php?id=kKGtuH:NJtZSB'
+    ).to_dict())
 
     # ROOMS #
 
@@ -94,8 +141,8 @@ if __name__ == "__main__":
         'Test Room',
         'discjockey',
         {'discjockey': user_list[0], 'test1': 1, 'test2': 2, 'test3': 3},
-        {'playlist': [Song(1, "Despacito", "Vida", ["Luis Fonsi", "Daddy Yankee"], "Pop","http://res.cloudinary.com/alick/video/upload/v1502689683/Luis_Fonsi_-_Despacito_ft._Daddy_Yankee_uyvqw9.mp3").to_dict(),
-         Song(2, "La Mujer de Antonio", "SONGO 21 - Studio sessions 2003", ["SONGO 21"], "Salsa", "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/SONGO_21/SONGO_21_-_Studio_sessions_2003/SONGO_21_-_02_-_La_Mujer_de_Antonio.mp3").to_dict()],
+        {'playlist': [Song(1, "Despacito", "Vida", ["Luis Fonsi", "Daddy Yankee"], "Pop", "http://res.cloudinary.com/alick/video/upload/v1502689683/Luis_Fonsi_-_Despacito_ft._Daddy_Yankee_uyvqw9.mp3").to_dict(),
+                      Song(2, "La Mujer de Antonio", "SONGO 21 - Studio sessions 2003", ["SONGO 21"], "Salsa", "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/SONGO_21/SONGO_21_-_Studio_sessions_2003/SONGO_21_-_02_-_La_Mujer_de_Antonio.mp3").to_dict()],
          'currentTime': 10}
     ).to_dict())
     room_list.append(Room(
